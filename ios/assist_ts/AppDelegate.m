569287e5efb9f5c3216f5402dab325fb0bcee950
ybalaev1 <balaev.ya@gazelkin.ru>
2021-10-11 11:02:03 +0300

    set main jsbundle appdelegate

--- ios/assist_ts/AppDelegate.m
+++ ios/assist_ts/AppDelegate.m
@@ -61,7 +61,7 @@ static void InitializeFlipper(UIApplication *application) {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [CodePush bundleURL];
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
